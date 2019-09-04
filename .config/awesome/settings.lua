bea33429a5d59da97e4244732d984ff5e83ad1f2
razorkitty <razorkitty@null.net>
2019-09-04 12:02:20 +0100

    added stettings file

---
+++ .config/awesome/settings.lua
@@ -0,0 +1,7 @@
+local settings = {}
+settings.terminal = 'st'
+settings.editor = 'nvim'
+settings.editor_cmd = settings.terminal..' -e '.. settings.editor
+settings.modkey = 'Mod4'
+
+return settings
